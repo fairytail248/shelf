56e7a944a6418242014e83fd8816d6ad846cc2c8
fairytail248 <venkatanarendra2002@gmail.com>
2025-05-02 17:53:34 +0530

    first commit

---
+++ shelf.js
@@ -0,0 +1,16 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+
+function sortFruit() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊"
+        }
+    }
+}
+
+sortFruit()
